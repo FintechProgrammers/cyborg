e3e9a3d165fca5a373cfd1f40592d5435e3e4a1c
DEv Noel <ndamjoh@gmail.com>
2024-01-19 10:45:01 +0100

    updated

--- app/Http/Resources/ProfitResource.php
+++ app/Http/Resources/ProfitResource.php
@@ -15,8 +15,8 @@ class ProfitResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'today_profit'  =>  empty($this->today_profit) ? $this->today_profit : 0.00 . 'USDT',
-            'total_profit'  =>  empty($this->total_profit) ? $this->total_profit : 0.00 . 'USDT'
+            'today_profit'  =>  empty($this->today_profit) ? $this->today_profit : 0.00,
+            'total_profit'  =>  empty($this->total_profit) ? $this->total_profit : 0.00'
         ];
     }
 }
