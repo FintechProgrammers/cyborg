37f07737ad4f806a415fdfde70979e5f5925b7ad
DEv Noel <ndamjoh@gmail.com>
2024-01-23 09:53:55 +0100

    updated

--- resources/views/admin/authentication/layouts/app.blade.php
+++ resources/views/admin/authentication/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DeltaCyborg | @yield('title')</title>
+    <title>{{ Config::get('app.name', 'Laravel') }} | @yield('title')</title>
 
     @include('admin.layouts.partials._styles')
 </head>
