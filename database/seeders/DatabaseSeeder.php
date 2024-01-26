c970e1cd01de0baeab62f51952f4aca205e9a492
DEv Noel <ndamjoh@gmail.com>
2024-01-26 11:41:04 +0100

    updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             MarketSeeder::class,
+            AdminSeeder::class,
             RoleAndPermissionSeeder::class,
             SettingsSeed::class
         ]);
