1a2bc869a64027fda4bcdf8b129dbf9288a626e2
DEv Noel <ndamjoh@gmail.com>
2024-01-28 20:32:06 +0100

    modified

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('admin.login');
+    return view('admin.authentication.login');
 });
 
 Route::post('coinpayment/ipn', CoinpaymentController::class);
