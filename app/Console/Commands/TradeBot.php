6a625494904bd7b58f56be64d63395abce66cf0d
DEv Noel <ndamjoh@gmail.com>
2024-02-05 20:20:52 +0100

    to 20%

--- app/Console/Commands/TradeBot.php
+++ app/Console/Commands/TradeBot.php
@@ -329,7 +329,7 @@ class TradeBot extends Command
             $check_balance = $settings->capital - 5;
 
             // calculate percentage of exchange balance
-            $balancePercentage = ($check_balance * 50) / 100;
+            $balancePercentage = ($check_balance * 20) / 100;
 
             // First entry
             if (!$in_position && $margin_calls === 0) {
